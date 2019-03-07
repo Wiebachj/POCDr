99c1e06adb4255020b03d418252f9ace279b7a9f
Wiebach <wiebachj@charite.de>
2019-03-07 06:48:11 +0100

    correct the worst performance function

--- R/POCD_functions_master.R
+++ R/POCD_functions_master.R
@@ -1,16 +1,15 @@
 #' calculation of change scores
 #'
-#' @param Input               data frame with poled CTPs
-#' @param Timepoint.names     names of the test timepoints
-#' @param Groups              Groups of Treatment used for the sublists e.g. Patients and Controls
+#' @param Input list of data frames with poled CTPs
+#' @param Timepoint.names names of the test timepoints
+#' @param Groups Groups of Treatment used for the sublists e.g. Patients and Controls
+#' @return list of data frames with calculated change scores
 #' @export calc.changeScore.FUN
 
-
-
-calc.changeScore.FUN <- function(Input  = Config$CogDat$CTP.Poled.missForest,
-                                 Timepoint.names = Config$parameter$names.Timepoints,
-                                 Groups = names(Config$parameter$names.Group)){
-
+calc.changeScore.FUN <- function(Input,#  = Config$CogDat$CTP.Poled.missForest,
+                                 Timepoint.names,# = Config$parameter$names.Timepoints,
+                                 Groups)# = names(Config$parameter$names.Group))
+  {
   CS <- Input
   for (i in Groups){
   df <- CS[grepl( i, names(CS))]
@@ -30,19 +29,18 @@ calc.changeScore.FUN <- function(Input  = Config$CogDat$CTP.Poled.missForest,
 
 #' corrected change scores
 #'
-#' @param Input               data frame with uncorrected change score of patients group
-#' @param meanControl         mean of control group, which is divided of change score
-#' @param Timepoint.names     names of the test timepoints
-#' @param CTP.names         names of CTP tests
+#' @param Input list of data frames with uncorrected change score of patients group
+#' @param Control list of data frames with uncorrected change score of control group
+#' @param Timepoint.names names of the test timepoints
+#' @param CTP.names names of CTP tests
+#' @return list of data frames with corrected change scores
 #' @export correct.changeScore.FUN
 
-
-
-correct.changeScore.FUN <- function(Input  = Config$CogDat$no_imp$CS.Delta$Patients,
-                                    Control = Config$CogDat$no_imp$CS.Delta$Controls,
-                                    Timepoint.names = Config$parameter$names.Timepoints,
-                                    CTP.names        = Config$parameter$colnames.CTPs){
-
+correct.changeScore.FUN <- function(Input,#  = Config$CogDat$no_imp$CS.Delta$Patients,
+                                    Control,# = Config$CogDat$no_imp$CS.Delta$Controls,
+                                    Timepoint.names,# = Config$parameter$names.Timepoints,
+                                    CTP.names)#        = Config$parameter$colnames.CTPs)
+  {
   CS <- list()
 
   calc.CS.FUN <- function(Timepoint){
@@ -56,30 +54,28 @@ correct.changeScore.FUN <- function(Input  = Config$CogDat$no_imp$CS.Delta$Patie
     return(CS)
   }
 
-
   CS <- lapply(Timepoint.names, calc.CS.FUN)
   CS <- unlist(CS, recursive = F)
 
   return(CS)
-
   }
 
 ############################################################################
 
 #' calculate Z- scores
 #'
-#' @param Input                data frame with uncorrected change score of patients group
-#' @param Control              control group
-#' @param Timepoint.names      names of the test timepoints
-#' @param CTP.names            names of CTP tests
+#' @param Input.all list of data frames with corrected change score of patients group
+#' @param Control.all list of data frames with corrected change score of control group
+#' @param Timepoint.names names of the test timepoints
+#' @param CTP.names names of CTP tests
+#' @return list of data frames with calculated z scores
 #' @export calc.Zscore.FUN
 
-
-
-calc.Zscore.FUN <- function (Input.all   = Config$CogDat$CTP.Poled.missForest[grep("Patients.T0", names(Config$CogDat$CTP.Poled.missForest))],
-                             Control.all = Config$CogDat$CTP.Poled.missForest[grep("Controls.T0", names(Config$CogDat$CTP.Poled.missForest))],
-                             Timepoint.all = "T0",
-                             CTPs.names        = Config$parameter$colnames.CTPs){
+calc.Zscore.FUN <- function (Input.all,#   = Config$CogDat$CTP.Poled.missForest[grep("Patients.T0", names(Config$CogDat$CTP.Poled.missForest))],
+                             Control.all,# = Config$CogDat$CTP.Poled.missForest[grep("Controls.T0", names(Config$CogDat$CTP.Poled.missForest))],
+                             Timepoint.names,# = "T0",
+                             CTP.names)#        = Config$parameter$colnames.CTPs)
+  {
   Zscore = list()
   Zcalc.FUN <- function(Timepoint){
 
@@ -87,12 +83,14 @@ calc.Zscore.FUN <- function (Input.all   = Config$CogDat$CTP.Poled.missForest[gr
   Control <- Control.all[[grep(Timepoint, names(Control.all))]]
 
 
-  Zscore[[Timepoint]] <- data.frame(sapply(grep(Timepoint, CTPs.names, value = T), function(i) { magrittr::subtract(Input[[i]], mean(Control[[i]], na.rm = T)) / sd(Control[[i]], na.rm = T)}), row.names = rownames(Input))
+  Zscore[[Timepoint]] <- data.frame(sapply(grep(Timepoint, CTP.names, value = T),
+                                            function(i) { magrittr::subtract(Input[[i]], mean(Control[[i]], na.rm = T)) / stats::sd(Control[[i]], na.rm = T)}),
+                          row.names = rownames(Input))
 
   return(Zscore)
   }
 
-  Zscore <- lapply(Timepoint.all, Zcalc.FUN)
+  Zscore <- lapply(Timepoint.names, Zcalc.FUN)
   Zscore <- unlist(Zscore, recursive = F)
 
   return(Zscore)
@@ -102,19 +100,17 @@ calc.Zscore.FUN <- function (Input.all   = Config$CogDat$CTP.Poled.missForest[gr
 ############################################################################
 #' calculate combined Z- scores
 #'
-#' @param Input                data frame containing RCI values for each CTP
-#' @param Control              control group
+#' @param Input.all list of data frames containing z score for each CTP of patients group
+#' @param Control.all list of data frames containing z score for each CTP of control group
+#' @param Timepoint.names names of the test timepoints
+#' @return list of data frames with combine scores
 #' @export calc.combined.Zscore.FUN
 
-
-
-calc.combined.Zscore.FUN <- function(Input.all  = Config$CogDat$no_imp$Zscore.Baseline.Patients,
-                                     Control.all = Config$CogDat$no_imp$Zscore.Baseline.Control,
-                                     Timepoint.all = "T0"
-                                     ){
-
-
-RCI <- list()
+calc.combined.Zscore.FUN <- function(Input.all,#  = Config$CogDat$imp$Zscore.Baseline.Patients,
+                                     Control.all,# = Config$CogDat$imp$Zscore.Baseline.Control,
+                                     Timepoint.names)# = "T0"
+  {
+  RCI <- list()
 
   Zcomb.FUN <- function (Timepoint) {
     Input   <- Input.all[[grep(Timepoint, names(Input.all))]]
@@ -122,13 +118,13 @@ RCI <- list()
 
 
   # take only subjects into account that have a complete testing
-  RCI.Complete = na.omit(Input)
-  RCI.Complete.Control = na.omit(Control)
+  RCI.Complete = stats::na.omit(Input)
+  RCI.Complete.Control = stats::na.omit(Control)
   # calculate the sum of single RCI scores for every subject with complete testing
   RCI.Sum = rowSums(RCI.Complete)
   RCI.Sum.Control = rowSums(RCI.Complete.Control)
   # calculate SD of RCI sum for the control group
-  RCI.SD = sd(RCI.Sum.Control)
+  RCI.SD = stats::sd(RCI.Sum.Control)
 
   # create template for new data frame
   RCI.Combined = RCI
@@ -137,32 +133,31 @@ RCI <- list()
   RCI.Combined[[Timepoint]] = data.frame(Zscore.combined = sapply(RCI.Sum, "/", RCI.SD), row.names = rownames(RCI.Complete))
   return(RCI.Combined)
 }
-  RCI.Combined <- lapply(Timepoint.all, Zcomb.FUN)
+  RCI.Combined <- lapply(Timepoint.names, Zcomb.FUN)
   RCI.Combined <- unlist(RCI.Combined, recursive = F)
   return(RCI.Combined)
 }
 
 ############################################################################
 #' calculate POCD criteria 7, 8, 9 according to Rasmussen et al. (2001)
-#'
-#' @param RCI.All                data frame containing RCI values for each CTP
-#' @param RCI.Combined           data frame containing combined RCI values for each CTP
-#' @param thresh              threshhold; Z values below this threshhold are classified as "hit"; default is -1.96
-#' @param hit                 how many "hits" are necessary for single POCD diagnosis; default is 2
-#' @return  POCD.Diagnosis: List with data frames containing final POCD diagnosis
+#' @description function for calculation POCD criteria 7, 8, 9 according to Rasmussen et al. (2001)
+#' @param RCI.All list of data frames containing z scores for each CTP
+#' @param RCI.Combined list of data frames containing combined z scores for each CTP
+#' @param Timepoint.names names of the test timepoints
+#' @param thresh threshhold; Z values below this threshhold are classified as "hit"; default is -1.96
+#' @param hit how many "hits" are necessary for single POCD diagnosis; default is 2
+#' @param Diagnose name of Diagnosis by a hit , e.g. PreCI or POCD
+#' @return  List with data frames containing final POCD diagnosis
 #' @export rci.diagnosis.FUN
 
-
 # function for calculation POCD criteria 7, 8, 9 according to Rasmussen et al. (2001)
-rci.diagnosis.FUN = function(RCI.All = Config$CogDat$no_imp$Zscore.Baseline.Patients,
+rci.diagnosis.FUN = function(RCI.All,# = Config$CogDat$imp$Zscore.Baseline.Patients,
                              thresh = -1.96,
                              hit = 2,
-                             RCI.Combined = Config$CogDat$no_imp$Zscore.combined.Baseline.Patients,
-                             Timepoint.all = "T0",
-                             Diagnose = "PreCI"){
-                             #RCI.Method,
-                             #label1 = "no POCD",
-                             #label2 = "POCD",...) {
+                             RCI.Combined,# = Config$CogDat$imp$Zscore.combined.Baseline.Patients,
+                             Timepoint.names,# = "T0",
+                             Diagnose = "PreCI")
+  {
 
   POCD.Diagnosis.9 <- list()
 
@@ -172,7 +167,7 @@ rci.diagnosis.FUN = function(RCI.All = Config$CogDat$no_imp$Zscore.Baseline.Pati
     RCI.comb <- RCI.Combined[[grep(Timepoint, names(RCI.Combined))]]
 
     # take only subjects into account that have a complete testing
-  RCI.Complete = na.omit(RCI)
+  RCI.Complete = stats::na.omit(RCI)
 
   # check if Z score is < -1.96
   POCD.Complete =  RCI.Complete < thresh
@@ -180,7 +175,10 @@ rci.diagnosis.FUN = function(RCI.All = Config$CogDat$no_imp$Zscore.Baseline.Pati
   # POCD criterium 7: deterioration in 2 or more CTPs < -1.96
   POCD.Diagnosis.7 = data.frame(
     SubjectID = row.names(POCD.Complete),
-    ZcoreHit  = rowSums(POCD.Complete) >= hit)
+    ZcoreHit  = rowSums(POCD.Complete) >= hit,
+    CTPs_hit  = sapply( 1:nrow(POCD.Complete), function(x) { paste( names(which( POCD.Complete[x, 1 : ncol(POCD.Complete)]  == TRUE)), collapse = " - " )})
+
+)
 
   # POCD criterium 8: combined Z score < -1.96
   POCD.Diagnosis.8 = data.frame(
@@ -188,13 +186,13 @@ rci.diagnosis.FUN = function(RCI.All = Config$CogDat$no_imp$Zscore.Baseline.Pati
     Zscore.combined  = RCI.comb < thresh)
 
   # POCD criterium 9: either deterioration in 2 or more CTPs < -1.96 or combined Z score < -1.96
-  POCD.Diagnosis.9[[Timepoint]] = plyr::join_all(list(POCD.Diagnosis.7, POCD.Diagnosis.8), by="SubjectID") %>% plyr::mutate(Diag = rowSums(.[2:3])>=1) %>% plyr::rename(c("Diag" = Diagnose))
+  POCD.Diagnosis.9[[Timepoint]] = plyr::join_all(list(POCD.Diagnosis.8, POCD.Diagnosis.7), by="SubjectID") %>% plyr::mutate(Diag = rowSums(.[2:3])>=1) %>% plyr::rename(c("Diag" = Diagnose))
   colnames(POCD.Diagnosis.9[[Timepoint]])[-1] <- paste0(Timepoint,".", colnames(POCD.Diagnosis.9[[Timepoint]])[-1])
   colnames(POCD.Diagnosis.9[[Timepoint]][]) <- paste0(Timepoint,".", colnames(POCD.Diagnosis.9[[Timepoint]])[-1])
   return(POCD.Diagnosis.9)
   }
 
-  POCD.Diagnosis.9 <- lapply(Timepoint.all,POCD.diagn.FUN)
+  POCD.Diagnosis.9 <- lapply(Timepoint.names,POCD.diagn.FUN)
   POCD.Diagnosis.9 <- unlist(POCD.Diagnosis.9, recursive = F)
   return(POCD.Diagnosis.9)
 
@@ -204,19 +202,24 @@ rci.diagnosis.FUN = function(RCI.All = Config$CogDat$no_imp$Zscore.Baseline.Pati
 
 #' calculate NCD
 #'
-#' @param Input                data frame CTPs
-#' @param Control              control group
-#' @param Timepoint.names      names of the test timepoints
-#' @param CTP.names            names of CTP tests
-#' @export calc.Zscore.FUN
+#' @param Input.all list fo data frames containing CTPs /RCIs of patients group
+#' @param Control.all list fo data frames containing CTPs/RCIs of control group
+#' @param Timepoint.names names of the test timepoints
+#' @param CTP.names names of CTP tests
+#' @param CTP.number numer of used CTP test
+#' @param impairment list of data frames containing the information about the impairment of the patients in logicals (TRUE, FALSE NA)
+#' @param NCD.names name of column containing the variables for impairment
+#' @return list of data frames comtaining NCD diagnosis for each patient
+#' @export calc.NCD.FUN
 
-
-
-calc.NCD.FUN <- function (   Input.all   = Config$CogDat$CTP.Poled.missForest[grep("Patients", names(Config$CogDat$CTP.Poled.missForest))],
-                             Control.all = Config$CogDat$CTP.Poled.missForest[grep("Controls", names(Config$CogDat$CTP.Poled.missForest))],
-                             impairment = Config$CogDat$CTPs.control.clean.interim[grep("Patients", names(Config$CogDat$CTPs.control.clean.interim))],
-                             Timepoint.all = "T0",
-                             CTPs.names        = Config$parameter$colnames.CTPs){
+calc.NCD.FUN <- function (   Input.all,#   = Config$CogDat$CTP.Poled.missForest[grep("Patients", names(Config$CogDat$CTP.Poled.missForest))],
+                             Control.all,# = Config$CogDat$CTP.Poled.missForest[grep("Controls", names(Config$CogDat$CTP.Poled.missForest))],
+                             impairment,# = Config$CogDat$CTPs.control.clean.interim[grep("Patients", names(Config$CogDat$CTPs.control.clean.interim))],
+                             Timepoint.names,# = "T0",
+                             CTP.names,#        = Config$parameter$colnames.CTPs,
+                             CTP.number,#       = Config$parameter$N.CTPs,
+                             NCD.names)#        = Config$parameter$colnames.NCD
+  {
   Score = list()
   NCDcalc.FUN <- function(Timepoint){
 
@@ -225,42 +228,50 @@ calc.NCD.FUN <- function (   Input.all   = Config$CogDat$CTP.Poled.missForest[gr
     Control <- Control.all[[grep(Timepoint, names(Control.all))]]
     Control <- Control[!is.na(rowSums(Control)),]
     imp     <- impairment[[grep(Timepoint, names(impairment))]]
+    #imp     <- imp[[grep(NCD.names, colnames(imp))]]
+    Input <- merge(x = Input, y = imp[, c("SubjectID", colnames(imp)[colnames(imp) %in% NCD.names])],
+                    by.x = "row.names", by.y = "SubjectID",
+                    all.x = T  )
 
-    Input <- merge(x = Input, y = imp[, c("SubjectID", colnames(imp)[colnames(imp) %in% Config$parameter$names.NCD])],
-                   by.x = "row.names", by.y = "SubjectID",
-                   all.x = T  )
 
+    Score[[Timepoint]] <- data.frame(sapply(grep(Timepoint, CTP.names, value = T), function(i) {
 
-    Score[[Timepoint]] <- data.frame(sapply(grep(Timepoint, CTPs.names, value = T), function(i) {
-
-      Score[[i]][Input[[i]] < magrittr::subtract(mean(Control[[i]], na.rm = T), sd(Control[[i]], na.rm = T)) &
-         Input[[i]] > magrittr::subtract(mean(Control[[i]], na.rm = T), 2*sd(Control[[i]], na.rm = T))] <- 1
-      Score[[i]][Input[[i]] <= magrittr::subtract(mean(Control[[i]], na.rm = T), 2*sd(Control[[i]], na.rm = T))] <- 2
-      Score[[i]][Input[[i]] >= magrittr::subtract(mean(Control[[i]], na.rm = T), sd(Control[[i]], na.rm = T))] <- 0
+      Score[[i]][Input[[i]] < magrittr::subtract(mean(Control[[i]], na.rm = T), stats::sd(Control[[i]], na.rm = T)) &
+         Input[[i]] > magrittr::subtract(mean(Control[[i]], na.rm = T), 2*stats::sd(Control[[i]], na.rm = T))] <- 1
+      Score[[i]][Input[[i]] <= magrittr::subtract(mean(Control[[i]], na.rm = T), 2*stats::sd(Control[[i]], na.rm = T))] <- 2
+      Score[[i]][Input[[i]] >= magrittr::subtract(mean(Control[[i]], na.rm = T), stats::sd(Control[[i]], na.rm = T))] <- 0
 
         return(Score[[i]])
         }), row.names = Input$Row.names)
 
-        Score[[Timepoint]] <- data.frame( score = apply(Score[[Timepoint]][,1:Config$parameter$N.CTPs], 1, max),
-                                          impairment = ifelse(Input[colnames(Input) %in% Config$parameter$names.NCD] == "impairment", 1, 0))
+    Score[[Timepoint]]$score <- apply(Score[[Timepoint]][,1: CTP.number], 1, max)
+    Score[[Timepoint]]$imp   <- Input[[grep("Impairment", colnames(Input))]]
 
-    Score[[Timepoint]]$NCD[Score[[Timepoint]][,1] == 2 & Score[[Timepoint]][,2] == 1] <- "major"
-    Score[[Timepoint]]$NCD[Score[[Timepoint]][,1] == 2 & Score[[Timepoint]][,2] == 0 |
-                           Score[[Timepoint]][,1] == 1 & Score[[Timepoint]][,2] == 1 |
-                           Score[[Timepoint]][,1] == 1 & Score[[Timepoint]][,2] == 0 ] <- "mild"
-    Score[[Timepoint]]$NCD[Score[[Timepoint]][,1] == 0 & Score[[Timepoint]][,2] == 0 |
-                           Score[[Timepoint]][,1] == 0 & Score[[Timepoint]][,2] == 1  ] <-   "no"
+        #Score[[Timepoint]] <- data.frame( score = apply(Score[[Timepoint]][,1: CTP.number], 1, max),
+        #                                  impairment = Score[[Timepoint]]$imp)
+
+    Score[[Timepoint]]$NCD[Score[[Timepoint]]$score == 2 & Score[[Timepoint]]$imp == TRUE] <- "major"
+
+    Score[[Timepoint]]$NCD[Score[[Timepoint]]$score == 2 & Score[[Timepoint]]$imp == FALSE |
+                           Score[[Timepoint]]$score == 1 & Score[[Timepoint]]$imp == TRUE  |
+                           Score[[Timepoint]]$score == 1 & Score[[Timepoint]]$imp == NA    |
+                           Score[[Timepoint]]$score == 1 & Score[[Timepoint]]$imp == FALSE ] <- "mild"
+
+    Score[[Timepoint]]$NCD[Score[[Timepoint]]$score == 0 & Score[[Timepoint]]$imp == TRUE |
+                           Score[[Timepoint]]$score == 0 & Score[[Timepoint]]$imp == NA    |
+                           Score[[Timepoint]]$score == 0 & Score[[Timepoint]]$imp == FALSE  ] <-   FALSE
+
+    Score[[Timepoint]]$NCD[Score[[Timepoint]]$score == 2 & Score[[Timepoint]]$imp == NA] <- NA
 
 
-colnames(Score[[Timepoint]]) <- c(paste0(Timepoint, c("_score","_impairment","_NCD" )))
+colnames(Score[[Timepoint]])[which(colnames(Score[[Timepoint]]) %in% c("score","imp","NCD"))] <- c(paste0(Timepoint, c("_score","_impair","_NCD" )))
 Score[[Timepoint]]$SubjectID <- row.names(Score[[Timepoint]])
     return(Score)
   }
 
-  NCDscore <- lapply(Timepoint.all, NCDcalc.FUN)
+  NCDscore <- lapply(Timepoint.names, NCDcalc.FUN)
   NCDscore <- unlist(NCDscore, recursive = F)
 
     return(NCDscore)
 
 }
-
--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' Parameters to be loaded
-#'
-#' Parameters to be loaded
-#'
-#' @format A list with useful parameters
-#' @source None
-"par"
